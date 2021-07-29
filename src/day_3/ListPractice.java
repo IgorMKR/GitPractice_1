b779648d01b17c846da6b20c532337a0a67a0e01
IgorMKR <sumz1305@gmail.com>
2021-07-29 11:39:22 -0700

    starte new practice 2

--- src/day_3/ListPractice.java
+++ src/day_3/ListPractice.java
@@ -1,4 +1,21 @@
 package day_3;
 
+import java.util.ArrayList;
+
 public class ListPractice {
-}
+
+    public static void main(String[] args) {
+
+    }
+
+    //start practice 2
+
+
+    }
+
+
+
+
+
+
+
