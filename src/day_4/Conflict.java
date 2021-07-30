318dbb937d8613ff6b84151e7b44114c597fba24
IgorMKR <sumz1305@gmail.com>
2021-07-30 07:27:09 -0700

    collection work started

---
+++ src/day_4/Conflict.java
@@ -0,0 +1,18 @@
+package day_4;
+
+public class Conflict {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is collection branch content");
+
+    }
+
+
+
+
+
+
+
+
+}
